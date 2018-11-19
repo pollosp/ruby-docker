46177faa0d71381742f0b7d3c144cdeaf1b3eda6
pollosp <pollo.es.pollo@gmail.com>
2018-11-19 21:31:39 +0100

    Fix file path for structure-tests

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/structure-tests.yaml/structure-tests.yaml:/tmp/tests.yaml gcr.io/gcp-runtimes/container-structure-test test --image sinatra --config /tmp/tests.yaml
+echo "Lintering"
 cat Dockerfile | docker run --rm -i hadolint/hadolint hadolint --ignore DL3018 --ignore SC2035 -
+echo "Structure tests"
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/structure-tests/structure-tests.yaml:/tmp/tests.yaml gcr.io/gcp-runtimes/container-structure-test test --image sinatra --config /tmp/tests.yaml
